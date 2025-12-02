1f747cb2d28bcc361463aa3e52364f8ef393f339
Michael Ou <shanou0601@gmail.com>
2025-12-01 20:45:57 -0500

    Add files via upload

---
+++ Lab 11.py
@@ -0,0 +1,29 @@
+
+
+import matplotlib.pyplot as plt
+import os
+
+
+class Student:
+    def __init__(self,name, id_num,grade):
+        self.name = name
+        self.id_num = id_num
+
+
+class Assignment:
+    def __init__(self,name,ass_id,point):
+        self.name = name
+        self.ass_id=ass_id
+        self.point = point
+
+class Submissions:
+    def __init__(self,id_num,ass_id,percent):
+        self.id_num = id_num
+        self.ass_id=ass_id
+        self.percent = percent
+
+    def
+
+
+
+
